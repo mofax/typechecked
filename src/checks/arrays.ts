5fb3edb5f0c03c1df7562dfb4417a83d8bb0e254
Alloys Mila <mofax@users.noreply.github.com>
2023-01-16 11:23:05 +0300

    Add support for checking tuples

--- src/checks/arrays.ts
+++ src/checks/arrays.ts
@@ -26,3 +26,50 @@ export function isArray<S extends (value: unknown) => any, C extends ReturnType<
         return checked;
     }
 }
+
+type ReturnTypeExtractor<T extends Readonly<ValidatorFunction<unknown>[]>, S extends ReturnType<T[number]>[] = []> =
+    T["length"] extends S["length"] ? S : ReturnTypeExtractor<T, [...S, ReturnType<T[S['length']]>]>
+
+/**
+ * Tuple validation  
+ * Takes in a tuple passed in (as const)  
+ * 
+ * For example
+ * 
+ * ```
+ *  const tupleCheck = isTuple(isString, isBoolean);
+ *  // tupleCheck is now a function that can be used for validation
+ * 
+ *  const checked = tupleCheck["true", false];
+ *  // checked is of type [string, boolean]
+ * 
+ *  tupleCheck(["true", false]); // this will pass
+ *  tupleCheck([43, "23"]); // this will fail
+ * 
+ * ```
+ * 
+ */
+export function isTuple<A extends Readonly<ValidatorFunction<unknown>[]>>(...arg: A) {
+
+    return function check(value: unknown) {
+
+        if (!Array.isArray(value)) {
+            throw new TypeCheckedError(`Expected value to be an array but found ${typeof value}`);
+        }
+
+        if (arg.length !== value.length) {
+            throw new TypeCheckedError(`Expected value to have length ${arg.length} tuple but found ${value.length}`);
+        }
+
+        arg.forEach((argItem, index) => {
+            try {
+                argItem(value[index])
+            } catch (err) {
+                const error = err as Error;
+                throw new TypeCheckedError(`Error at ${index}: ${error.message}`)
+            }
+        })
+
+        return value as ReturnTypeExtractor<A>
+    }
+}
